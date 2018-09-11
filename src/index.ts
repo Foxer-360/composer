181c0a6e49f3b256a810e8a699202b0005c541f4
Filip Suchý <nevim42@gmail.com>
2018-09-11 19:18:31 +0200

    Exported some definitions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import Composer from '@source/scenes/Composer';
+import {
+  IAddComponentObject,
+  IComponentObject,
+  IComponentsServiceLikeClass,
+  IData,
+  IEditorInfo,
+  ILockInfo,
+  IPluginServiceLikeClass,
+} from '@source/scenes/Composer/Composer';
 import LightweightComposer from '@source/scenes/LightweightComposer';
 
 export {
   Composer,
   LightweightComposer,
+
+  IAddComponentObject,
+  IComponentObject,
+  IComponentsServiceLikeClass,
+  IData,
+  IEditorInfo,
+  ILockInfo,
+  IPluginServiceLikeClass,
 };
