4f70b8960a5aafb32f41d8fc9afacd7e7d86dfde
Ondřej Janáček <janacek@centrum.cz>
2018-10-19 16:14:12 +0200

    Propagation language code to components

--- src/scenes/LightweightComposer/LightweightComposer.tsx
+++ src/scenes/LightweightComposer/LightweightComposer.tsx
@@ -37,6 +37,9 @@ class LightweightComposer extends React.Component<IProperties, {}> {
   }
 
   public render(): JSX.Element {
+    // tslint:disable-next-line:no-console
+    console.log('LightweightComposer props', this.props);
+
     return (
       <Container
         content={this.props.content.content}
--- src/scenes/LightweightComposer/components/Container/Container.tsx
+++ src/scenes/LightweightComposer/components/Container/Container.tsx
@@ -35,11 +35,17 @@ class Container extends React.Component<IProperties, {}> {
         const Comp = this.props.componentModule.getComponent(node.name);
         const navigations = this.props.context.readProperty('navigations');
         const languages = this.props.context.readProperty('languages');
+        const languageCode = this.props.context.readProperty('languageCode');
+
+        // tslint:disable-next-line:no-console
+        console.log('Container languageCode', languageCode);
+
         return (
           <Comp
             data={node.data}
             navigations={navigations}
             languages={languages}
+            languageCode={languageCode}
             key={node.id}
           />
         );
